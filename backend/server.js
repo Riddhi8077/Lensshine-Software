3ed53ea5820a6fce09667fd0843d1728aac63ba2
Riddhi <Riddhipachehara@gmail.com>
2026-05-03 16:55:17 +0530

    fixed emailing issue by changing port number

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,8 @@ app.use(
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
