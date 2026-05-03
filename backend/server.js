c4961156164c9ef833b1b23499186ef4e5e89770
Riddhi <Riddhipachehara@gmail.com>
2026-05-03 12:23:34 +0530

    updated origins to allow multiple frontend urls

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,8 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
-].filter(Boolean);
-
-const cors = require("cors");
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
   "https://lensshinesoftware.netlify.app"
-];
+].filter(Boolean);
 
 app.use(
   cors({
